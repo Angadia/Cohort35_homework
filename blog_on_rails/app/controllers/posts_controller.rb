ef907989eccdb1f84766e8710b40c42f1c18bd1d
Jignesh Patel <ca.jignesh.angadia@gmail.com>
2020-01-03 23:02:51 -0800

    Added CRUD for Posts.

--- blog_on_rails/app/controllers/posts_controller.rb
+++ blog_on_rails/app/controllers/posts_controller.rb
@@ -1,5 +1,49 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all.order("updated_at DESC")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new posts_params
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update posts_params
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_post
+    @post = Post.find params[:id]
+  end
+
+  def posts_params
+    params.require(:post).permit(:title, :body)
   end
 end
