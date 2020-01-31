e3a048f113bff63c31bd6d4697fa3529dc84dea9
Jignesh Patel <ca.jignesh.angadia@gmail.com>
2020-01-30 23:08:53 -0800

    Added functionality to show incorrect guesses hangman images; Added the winning and losing sounds with alerts.

--- hangman/js/main.js
+++ hangman/js/main.js
@@ -1,31 +1,64 @@
 $(document)
   .ready( () => {
     
-    const mysteryWord = ["ROCK", "PAPER", "SCISSORS"][Math.floor(Math.random()*3)];
+    const MAX_INCORRECT_TRIES = 7;
     
-    function createMysteryWordTableEntries(mysteryWord) {
+    const MYSTERY_WORD = ["ROCK", "PAPER", "SCISSORS"][Math.floor(Math.random()*3)];
+
+    const applauseSound = new Audio('sounds/applause3.wav');
+    const bombSound = new Audio('sounds/bomb_x.wav');
+    
+    let incorrectGuessCount = 0;
+    let correctGuessCount = 0;
+    
+    function createMysteryWordTableEntries(MYSTERY_WORD) {
       returnHTML = '<tr>';
-      for (let index = 0; index < mysteryWord.length; index++) {
-        returnHTML += `<td>${mysteryWord[index]}</td>`;
+      for (let index = 0; index < MYSTERY_WORD.length; index++) {
+        returnHTML += `<td>${MYSTERY_WORD[index]}</td>`;
       }
       returnHTML += '</tr>';
       return returnHTML;
     };
 
-    $('table.mysteryWord').prepend($(createMysteryWordTableEntries(mysteryWord)));
+    $('table.mysteryWord').prepend($(createMysteryWordTableEntries(MYSTERY_WORD)));
+
+    function updateTheImageForIncorrectGuess() {
+      if (incorrectGuessCount < MAX_INCORRECT_TRIES) {
+        $('img.hangmanImage').attr('src', `images/${incorrectGuessCount}.jpg`);
+      }
+      if (incorrectGuessCount+1 >= MAX_INCORRECT_TRIES) {
+        bombSound.play();
+        setTimeout(() => {
+          $(alert("Better luck next time..."));
+          location.reload();
+        }, 5000);
+      }
+    };
     
     function checkTheGuess(jQeCT) {
       let notFound = true;
-      for (let index = 0; index < mysteryWord.length; index++) {
-        if (jQeCT.text() === mysteryWord[index]) {
+      for (let index = 0; index < MYSTERY_WORD.length; index++) {
+        if (jQeCT.text() === MYSTERY_WORD[index]) {
           notFound = false;
-          $(`table.mysteryWord td:nth-child(${index+1})`).css("color", "grey");
+          correctGuessCount++;
+          $(`table.mysteryWord td:nth-child(${index+1})`).css("color", "black");
         }
       }
+
       if (notFound) {
-        console.log('Not found ', jQeCT.text())
+        incorrectGuessCount++;
+        updateTheImageForIncorrectGuess();
       }
-    }
+      
+      if (correctGuessCount == MYSTERY_WORD.length) {
+        applauseSound.play();
+        setTimeout(() => {
+          $(alert("Congratulations! You Won!"));
+          location.reload();
+        }, 5000);
+      }
+    };
+
     $('table.alphabets td').on('click', e => {
       const jQeCT = $(e.currentTarget);
       if (!jQeCT.hasClass('selected')) {
